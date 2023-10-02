85e5a15dc80c8262a955b6e67d73395523235bdd
avideb24 <debavi24@gmail.com>
2023-10-02 16:29:35 +0600

    header search btn and box completed

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { AiOutlineUser, AiOutlineHeart, AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
+
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -27,7 +32,7 @@ const Header = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                    <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/'>Home</Link></li>
                         <li tabIndex={0}>
                             <details>
                                 <summary>Category</summary>
@@ -42,8 +47,34 @@ const Header = () => {
                         <li><Link to='/contact'>Contact Us</Link></li>
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+                <div className="navbar-end flex gap-3">
+                    <div className="relative">
+                        <button onClick={() => setIsSearchVisible(!isSearchVisible)} className="text-2xl p-1"><AiOutlineSearch></AiOutlineSearch></button>
+                        {
+                            isSearchVisible ?
+                                <div className="absolute top-9 -left-44 flex w-52 h-8 duration-500 z-50">
+                                    <input className="w-[85%] h-full px-2 outline-none text-sm border-2 border-yellow-500" type="text" placeholder="Search here..." />
+                                    <button className="w-[15%] h-full bg-yellow-500"><span className="inline-block mx-auto text-white font-extrabold"><AiOutlineSearch></AiOutlineSearch></span></button>
+                                </div>
+                                :
+                                <div className="absolute top-9 -left-44 flex w-52 h-0 invisible duration-500 z-50">
+                                    <input className="w-[85%] h-full px-2 outline-none text-sm border-2 border-yellow-500" type="text" placeholder="Search here..." />
+                                    <button className="w-[15%] h-full bg-yellow-500"><span className="inline-block mx-auto text-white font-extrabold"><AiOutlineSearch></AiOutlineSearch></span></button>
+                                </div>
+                        }
+                    </div>
+                    <div className="relative">
+                        <button className=" text-2xl  p-1"><AiOutlineHeart></AiOutlineHeart></button>
+                        <p className="text-xs text-white text-center bg-yellow-500 w-4 h-4 absolute -top-1 -right-1 rounded-full">2</p>
+                    </div>
+                    <div className="relative">
+                        <button className=" text-2xl  p-1"><AiOutlineShoppingCart></AiOutlineShoppingCart></button>
+                        <p className="text-xs text-white text-center bg-yellow-500 w-4 h-4 absolute -top-1 -right-1 rounded-full">2</p>
+                    </div>
+                    <div className="relative">
+                        <button className="text-2xl p-1"><AiOutlineUser></AiOutlineUser></button>
+                        <p className="text-xs text-white text-center bg-yellow-500 w-4 h-4 absolute -top-1 -right-1 rounded-full">2</p>
+                    </div>
                 </div>
             </div>
         </div>
